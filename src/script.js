85bf4ac0740a2396252bf96fac35ff5c084f8384
Samir Jadhav <samirzjadhav@gmail.com>
2023-08-29 21:39:08 +0530

    feat: added classes

---
+++ src/script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const result = document.getElementById("result");
+const searchBtn = document.getElementById("search-btn");
+let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
