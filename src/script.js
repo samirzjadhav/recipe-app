6a4ca363c0832bb5dd6c964b99ea2bec81dd7ed6
Samir Jadhav <samirzjadhav@gmail.com>
2023-08-29 21:40:47 +0530

    feat: add script on searchbtn and fetch bata fom theMealDM

--- src/script.js
+++ src/script.js
@@ -3,3 +3,22 @@
 const result = document.getElementById("result");
 const searchBtn = document.getElementById("search-btn");
 let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+searchBtn.addEventListener("click", () => {
+  let userInp = document.getElementById("user-inp").value;
+  console.log(userInp);
+  if (userInp.length === 0) {
+    result.innerHTML = `<h3>Input Field Cannot Be Empty 😭</h3>`;
+  } else {
+    fetch(url + userInp)
+      .then((response) => response.json())
+      .then((data) => {
+        let myMeal = data.meals[0];
+        console.log(myMeal);
+        console.log(myMeal.strMealThumb);
+        console.log(myMeal.strMeal);
+        console.log(myMeal.strArea);
+        console.log(myMeal.strInstructions);
+      });
+  }
+});
