e1a07cfaef52443c0e3ab20f7c078dd2ec005de0
Samir Jadhav <samirzjadhav@gmail.com>
2023-08-29 21:42:18 +0530

    feat: add sript for take ingredient from theMealDM

--- src/script.js
+++ src/script.js
@@ -20,5 +20,21 @@ searchBtn.addEventListener("click", () => {
         console.log(myMeal.strArea);
         console.log(myMeal.strInstructions);
       });
+
+    let count = 1;
+    console.log(count);
+    let ingredients = [];
+
+    for (let i in myMeal) {
+      let ingredient = "";
+      let measure = "";
+      if (i.startsWith("strIngredient") && myMeal[i]) {
+        ingredient = myMeal[i];
+        measure = myMeal[`strMeasure` + count];
+        count += 1;
+        ingredients.push(`${measure} ${ingredient}`);
+      }
+    }
+    console.log(ingredients);
   }
 });
